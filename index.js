fce65a45aff51cd720dddf6c2f8c948a8a244ab7
Nathan Scheele <nathanscheele@gmail.com>
2019-11-12 13:43:28 -0500

    basic server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const browserSync = require('browser-sync');
+const path = require('path');
+
+let app = express();
+let port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+}).listen(port, listening);
+
+function listening () {
+  browserSync({
+    proxy: 'localhost:' + port,
+    files: ['public/**/*.{js,html}']
+  });
+}
