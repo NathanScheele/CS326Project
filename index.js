ed444aa116961fb9b45c67999981ab07e3eb1bd9
Nathan Scheele <nathanscheele@gmail.com>
2019-11-12 13:48:35 -0500

    moved directories around

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let app = express();
 let port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/html/index.html'));
 }).listen(port, listening);
 
 function listening () {
