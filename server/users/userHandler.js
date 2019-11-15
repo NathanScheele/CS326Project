b3e196d5e54f50c37a150e379aac6415c8ff7b60
Nathan Scheele <nathanscheele@gmail.com>
2019-11-15 16:22:52 -0500

    Sign-in and Sign-up

--- server/users/userHandler.js
+++ server/users/userHandler.js
@@ -1,30 +1,76 @@
-const User = require('./userModel.js')
+const User = require('./userModel.js');
+const jwt = require('jwt-simple');
+const passwordHash = require('password-hash');
+
+const secret = process.env.JWT_SECRET
 
 module.exports = {
-    // signin method
-    signin: function(req, res){
-      let userObj = req.body;
-      User.find(userObj, function(err, user){
-        if (err) { // notifies if error is thrown
-          console.log("mongo find user err: ", err);
-        }
-        else {
-          console.log("login success!")
-        }
-      });
-    },
+  // signin method
+  signin: function(req, res){
+    let name = req.body.username;
+    let pwd = req.body.password;
 
-    signup: function(req, res){
-      let newUserObj = req.body
-      User.create(newUserObj, function(err, user){
-        if (err) { // sign up error
-            console.log("mongo create user err: ", err);
-            res.status(500).send(err);
-        } 
-        else { // signup success
-            console.log("mongo create user success!")
-            res.status(200).send(newUserObj)
+    //Looks up the username to get the hashed password
+    User.find({username: name}, function(err, user){
+      if (err) { //Throws an error if something goes wrong with contacting Mongo
+        console.log("mongo find user err: ", err);
+      }
+      else {
+        if(user.length == 1){//If the username is found, verify the given password with the hashed password from Mongo
+          if(passwordHash.verify(pwd,user[0].password)){
+            console.log("Login success!");
+            let token = jwt.encode({username: user.username}, secret);
+            //Responds with a jwt token and the mongo_id
+            res.status(200).json({token: token, id: user[0].id})
+          }
+          else{//throw an error if the password is incorrect
+            console.log("ERROR: invalid password!");
+            res.status(500).send("ERROR: invalid password!");
+          }
         }
-      });
-    }
-  };
+        else{//throw an error if the 
+          console.log("Error: invalid username!")
+          res.status(500).send("Error: invalid username!");
+        }
+      }
+    });
+  },
+
+  signup: function(req, res){
+    let name = req.body.username;
+    let pwd = req.body.password;
+
+    //Checks to see if the given username is already in use
+    User.find({ username: name }).exec( function(err, user){
+      if (err) { //throw an error if something goes wrong when contacting the database
+        console.log("mongo find user err: ", err);
+        res.status(500).send(err);
+      }
+      else {
+        //user.length will be 0 when the username is not in the databse
+        if(user.length == 0){
+          let hashedPassword = passwordHash.generate(pwd);
+          
+          //Creates a new user with the given username and hashed password
+          User.create({username: name, password: hashedPassword}, function(err, user){
+            if (err) { //throws errow if something goes wrong when contacting the database
+              console.log("mongo create user err: ", err);
+              res.status(500).send(err);
+            } 
+            else {
+              console.log("mongo create user success!")
+              let token = jwt.encode({username: user.username}, secret);
+              //Responds with a jwt token and the mongo_id
+              res.status(200).json({token: token, id: user.id})
+            }
+          });
+        }
+        else{
+          //Throws an error if the username is already in use
+          console.log("Error: Username is taken!")
+          res.status(500).send("Username is taken")
+        }
+      }
+    });
+  }
+};
