c72a8c888b930931311ad909f3d5d53747274b7d
Matthew Spahl <mspahl@umass.edu>
2019-11-19 13:00:04 -0500

    updated serverContact

--- public/JS/myTable.js
+++ public/JS/myTable.js
@@ -1,3 +1,4 @@
+//get all of the data for the user when you open the page
 $(document).ready(function(){
     $.ajax({ url: "http://localhost:3000/api/signup",
             context: document.body,
--- public/JS/serverContact.js
+++ public/JS/serverContact.js
@@ -81,3 +81,12 @@ $("#itemConfirm").click(function() {
         $("#error").html("The user could not be registered.");
     });
 });
+
+//get all of the data for the user when you open the page
+$(document).ready(function(){
+    $.ajax({ url: "http://localhost:3000/api/signup",
+            context: document.body,
+            success: function(){
+               alert("done");
+            }});
+});
