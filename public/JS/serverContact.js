1ad9981cbf92b800cc62a5a5d27b3a00c6d64c2e
Nathan Scheele <nathanscheele@gmail.com>
2019-11-19 13:28:46 -0500

    fixed links

--- public/JS/serverContact.js
+++ public/JS/serverContact.js
@@ -74,9 +74,9 @@ $("#itemConfirm").click(function() {
 
 //get all of the data for the user when you open the page
 $(document).ready(function(){
-    $.ajax({ url: "http://localhost:3000/api/signup",
-            context: document.body,
-            success: function(){
-               alert("done");
-            }});
+    // $.ajax({ url: "http://localhost:3000/api/signup",
+    //         context: document.body,
+    //         success: function(){
+    //            alert("done");
+    //         }});
 });
